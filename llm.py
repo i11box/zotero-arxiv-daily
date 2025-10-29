68b62dbacea44a658b066ebd34ea74c89d140b97
i11box <1259084194@qq.com>
2025-10-29 23:54:01 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -11,8 +11,8 @@ class LLM:
             self.llm = OpenAI(api_key=api_key, base_url=base_url)
         else:
             self.llm = Llama.from_pretrained(
-                repo_id="Qwen/Qwen2.5-3B-Instruct-GGUF",
-                filename="qwen2.5-3b-instruct-q4_k_m.gguf",
+                repo_id="unsloth/gemma-3-4b-it-GGUF",
+                filename="gemma-3-4b-it-Q4_1.gguf",
                 n_ctx=5_000,
                 n_threads=4,
                 verbose=False,
